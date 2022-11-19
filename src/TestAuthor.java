1888dc22b101bc0af5c23ba7bea4f3c0ea25fd27
Andrei Cimpoeru <andrei.cimpoeru@bahiasoftware.es>
2022-11-19 17:45:23 +0100

    Initial commit

---
+++ src/TestAuthor.java
@@ -0,0 +1,10 @@
+public class aaa {
+
+    public static void main(String[] args) {
+        Author anAuthor = new Author("Tan Ah Teck", "ahteck@somewhere.com", 'm');
+        anAuthor.toString()
+    }
+
+
+
+}
